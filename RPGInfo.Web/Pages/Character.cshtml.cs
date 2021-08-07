e4a753211766ddec7e52847a312aaa875813d3e1
nickguerra <nickg@maliciousgames.com>
2021-08-07 15:28:18 -0500

    Added character page stub

---
+++ RPGInfo.Web/Pages/Character.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.Pages
+{
+    public class CharacterModel : PageModel
+    {
+        public void OnGet()
+        {
+            // TODO: List of characters when page opens
+            // Button to Add Character
+            // Form to include character properties 
+            // Define character properties in model
+            // reference https://www.youtube.com/watch?v=68towqYcQlY&t=3457s
+
+        }
+    }
+}
