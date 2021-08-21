1506acb4e3436b98cf48456da12c96003ace7211
nickguerra <nickg@maliciousgames.com>
2021-08-21 06:29:27 -0500

    connected model to detail page

---
+++ RPGInfo.Web/Pages/CharacterDetail.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RPGInfo.Web.Data;
+using RPGInfo.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.Pages
+{
+    public class CharacterDetail : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CharacterDetail(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Character Character { get; set; }
+
+
+        public void OnGet(int id)
+        {
+            Character = _context.Characters.Where(x => x.Id == id).FirstOrDefault();
+        }
+    }
+}
