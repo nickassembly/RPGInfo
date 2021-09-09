01d82731f9738b60b52f73837897105f2171227a
nickguerra <nickg@maliciousgames.com>
2021-09-09 06:21:22 -0500

    Added Related Character View Model

---
+++ RPGInfo.Web/ViewModels/RelatedCharacterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using RPGInfo.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.ViewModels
+{
+    public class RelatedCharacterViewModel
+    {
+        public byte[] RelatedCharacterPortrait { get; set; }
+        public string RelatedCharacterName { get; set; }
+        public string RelatedCharacterRace { get; set; }
+        public string RelatedCharacterClass { get; set; }
+        public string RelatedCharacterLocation { get; set; }
+        public Note RelatedCharacterNotes { get; set; }
+      
+    }
+}
