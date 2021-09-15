c0f2dea2389ca4e0754446af7fb78fc79d0d02fe
nickguerra <nickg@maliciousgames.com>
2021-09-15 09:19:40 -0500

    Added demo pizza object as a test case

---
+++ RPGInfo.Web/Models/PizzaSize.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.Models
+{
+    public enum PizzaSize
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ RPGInfo.Web/Pages/PizzaModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.Pages
+{
+    public class PizzaModel : PageModel
+    {
+       public void OnGet()
+        {
+
+        }
+    }
+}
