6e007a640d145897f94d49cd43e6716f6ce0d208
nickguerra <nickg@maliciousgames.com>
2021-10-28 07:00:53 -0500

    Added stub for 2 form handlers on character detail page

--- RPGInfo.Web/Pages/CharacterDetail.cshtml.cs
+++ RPGInfo.Web/Pages/CharacterDetail.cshtml.cs
@@ -20,20 +20,23 @@ namespace RPGInfo.Web.Pages
         {
             _context = context;
         }
-
-        [BindProperty]
-        public NoteViewModel NoteViewModel { get; set; }
-
+        // for character details (this page)
         [BindProperty]
         public Character Character { get; set; }
+
+        // properties for add note / model
+        [BindProperty]
+        public NoteViewModel NoteViewModel { get; set; }
+        [BindProperty]
+        public List<Note> CharacterNotes { get; set; } = new List<Note>();
+
+        // properties for add other characters / model
         public List<Character> KnownCharacterList { get; set; } = new List<Character>();
 
         [BindProperty]
         public int[] SelectedKnownCharacters { get; set; }
         public SelectList KnownCharacterOptions { get; set; }
 
-        [BindProperty]
-        public List<Note> CharacterNotes { get; set; } = new List<Note>();
 
 
         public void OnGet(int id)
@@ -54,6 +57,17 @@ namespace RPGInfo.Web.Pages
 
             return RedirectToPage("Note", new { id = id }); 
         }
+
+        public void OnPostAddNote()
+        {
+            // TODO: Enter details for adding note (connected to note models)
+        }
+
+        public void OnPostAddKnownCharacter()
+        {
+            // TODO: Enter details for adding character (connected to character models)
+        }
+
 
 
     }
