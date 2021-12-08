e17c0ae242cf174b98686827e28019d98fd9bb74
nickguerra <nickg@maliciousgames.com>
2021-12-08 05:48:02 -0600

    Reverted changes from test code

--- RPGInfo.Web/Pages/CharacterDetail.cshtml.cs
+++ RPGInfo.Web/Pages/CharacterDetail.cshtml.cs
@@ -20,19 +20,11 @@ namespace RPGInfo.Web.Pages
         private readonly ApplicationDbContext _context;
         private readonly ILogger<CharacterDetailModel> _logger;
 
-        public CharacterDetailModel(/*ApplicationDbContext context,*/ ILogger<CharacterDetailModel> logger)
+        public CharacterDetailModel(ApplicationDbContext context, ILogger<CharacterDetailModel> logger)
         {
-          //  _context = context;
+            _context = context;
             _logger = logger;
         }
-
-        // *** TODO TEST DATA *******
-        // Test data from SO Answer https://stackoverflow.com/questions/64334126/asp-net-core-3-1-razor-pages-adding-different-child-objects-to-header
-        // Added code for original Question, need to try solution implementation.
-        [BindProperty]
-        public Header MyHeader { get; set; } = new Header();
-
-
 
         // for character details (this page)
         [BindProperty]
@@ -45,107 +37,9 @@ namespace RPGInfo.Web.Pages
         {
             Character = _context.Characters.Where(x => x.Id == id).FirstOrDefault();
 
-            Character.CharacterNotes = _context.Notes.Where(x => x.NoteAuthor == Character.Name).ToList();
-
-            // Test data
-            MyHeader.Id = 1;
-            MyHeader.MyHeaderProperty = "HeaderTest1";
-            MyHeader.ChildOfHeader.Add(new ChildOfHeader());
-
-            for (int i = 1; i <= 3; i++)
-            {
-                var childOfChild = new ChildOfChild()
-                {
-                    Id = i,
-                    HeaderId = MyHeader.Id,
-                    MyChildProperty = $"FirstChildTest{i}"
-                };
-
-                MyHeader.ChildOfHeader[0].MyFirstChildList.Add(childOfChild);
-            }
-
-            for (int i = 1; i <= 2; i++)
-            {
-                var childOfChild = new ChildOfChild()
-                {
-                    Id = i,
-                    HeaderId = MyHeader.Id,
-                    MyChildProperty = $"SecondChildTest{i}"
-                };
-
-                MyHeader.ChildOfHeader[0].MySecondChildList.Add(childOfChild);
-            }
+            Character.CharacterNotes = _context.Notes.Where(x => x.NoteAuthor == Character.Name).ToList();   
 
         }
-
-        // Test data Partial View Results
-        public PartialViewResult OnPostAddNewFirstListChildItem([FromBody] Header myHeader)
-        {
-            if (myHeader.ChildOfHeader[0].MyFirstChildList == null)
-                myHeader.ChildOfHeader[0].MyFirstChildList = new List<ChildOfChild>();
-
-            var childId = myHeader.ChildOfHeader[0].MyFirstChildList.Count + 1;
-
-            myHeader.ChildOfHeader[0].MyFirstChildList.Add(new ChildOfChild
-            {
-                Id = childId,
-                HeaderId = myHeader.Id,
-                MyChildProperty = $"FirstChildTest{childId}"
-            });
-
-            var partialView = "_ListPartialView";
-            //var partialView = "_FirstListItemPartial";
-
-            var data = new CharacterDetailModel(_logger);
-            data.MyHeader = myHeader;
-
-            var myViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { { partialView, myHeader } };
-            //var myViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { { partialView, myHeader.ChildOfHeader[0].MyFirstChildList } };
-            myViewData.Model = data;
-
-            var partialViewResult = new PartialViewResult()
-            {
-                ViewName = partialView,
-                ViewData = myViewData,
-            };
-
-            return partialViewResult;
-        }
-
-        public PartialViewResult OnPostAddNewSecondListChildItem([FromBody] Header myHeader)
-        {
-            if (myHeader.ChildOfHeader[0].MySecondChildList == null)
-                myHeader.ChildOfHeader[0].MySecondChildList = new List<ChildOfChild>();
-
-            var childId = myHeader.ChildOfHeader[0].MySecondChildList.Count + 1;
-
-            myHeader.ChildOfHeader[0].MySecondChildList.Add(new ChildOfChild
-            {
-                Id = childId,
-                HeaderId = myHeader.Id,
-                MyChildProperty = $"SecondChildTest{childId}"
-            });
-
-            var partialView = "_ListPartialView";
-            //var partialView = "_SecondListItemPartial";
-
-            var data = new CharacterDetailModel(_logger);
-            data.MyHeader = myHeader;
-
-            var myViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { { partialView, myHeader } };
-            //var myViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), new ModelStateDictionary()) { { partialView, myHeader.ChildOfHeader[0].MySecondChildList } };
-            myViewData.Model = data;
-
-            var partialViewResult = new PartialViewResult()
-            {
-                ViewName = partialView,
-                ViewData = myViewData,
-            };
-
-            return partialViewResult;
-        }
-
-
 
         public IActionResult OnGetAddNote(int id)
         {
