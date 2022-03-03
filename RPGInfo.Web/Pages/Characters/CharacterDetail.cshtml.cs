2502a006c7b1da3ffa4551bfd0e7e9e094e8c080
nickguerra <nickg@maliciousgames.com>
2022-03-03 06:33:02 -0600

    Updated character display html

--- RPGInfo.Web/Pages/Characters/CharacterDetail.cshtml.cs
+++ RPGInfo.Web/Pages/Characters/CharacterDetail.cshtml.cs
@@ -94,13 +94,11 @@ namespace RPGInfo.Web.Pages
                 string titleKey = "Title:";
                 string dateKey = "Date:";
                 string contentKey = "Note:";
-              //  string buttonTextKey = "Remove";
                 int stringLength = stringsToConvert[i].Length;
 
                 int titleIndex = stringsToConvert[i].IndexOf(titleKey);
                 int dateIndex = stringsToConvert[i].IndexOf(dateKey);
                 int contentIndex = stringsToConvert[i].IndexOf(contentKey);
-               // int buttonIndex = stringsToConvert[i].IndexOf(buttonTextKey);
 
                 string extractedTitle = stringsToConvert[i][titleIndex..dateIndex];
                 string extractedDate = stringsToConvert[i][dateIndex..contentIndex];
