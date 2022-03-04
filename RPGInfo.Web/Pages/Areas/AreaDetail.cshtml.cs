5ef9e7255a4db9c0fa9630fb600f315aa18e7e74
nickguerra <nickg@maliciousgames.com>
2022-03-04 06:51:54 -0600

    Added models for area list and area detail pages

---
+++ RPGInfo.Web/Pages/Areas/AreaDetail.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGInfo.Web.Pages
+{
+    public class AreaDetailModel : PageModel
+    {
+        
+    }
+}
--- RPGInfo.Web/Pages/Areas/AreaList.cshtml.cs
+++ RPGInfo.Web/Pages/Areas/AreaList.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RPGInfo.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,13 @@ namespace RPGInfo.Web.Pages
 {
     public class AreaListModel : PageModel
     {
-        
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _environment;
+
+        public AreaListModel(ApplicationDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
     }
 }
