ae3a203cce08a435aa7df49f75fddc8a693278d8
nickguerra <nickg@maliciousgames.com>
2022-03-05 05:53:32 -0600

    Added Area List properties for code behind

--- RPGInfo.Web/Pages/Areas/AreaList.cshtml.cs
+++ RPGInfo.Web/Pages/Areas/AreaList.cshtml.cs
@@ -1,9 +1,15 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RPGInfo.Web.Data;
+using RPGInfo.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +24,64 @@ namespace RPGInfo.Web.Pages
         {
             _context = context;
             _environment = environment;
+        }
+
+        [BindProperty]
+        public Area Area { get; set; }
+
+        [NotMapped]
+        [BindProperty]
+        public IFormFile AreaImage { get; set; }
+
+        public List<Area> AreaList { get; set; } = new List<Area>();
+
+        public void OnGet()
+        {
+            AreaList = _context.AreasOfInterest.ToList();
+        }
+
+        public IActionResult OnPostDelete(int id)
+        {
+            var area = _context.AreasOfInterest.Where(x => x.Id == id).FirstOrDefault();
+
+            _context.Remove(area);
+            _context.SaveChanges();
+
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            DateTimeOffset createdDate = DateTime.Now;
+
+            var area = Area;
+            area.AreaName = Area.AreaName;
+            area.AreaDescription = Area.AreaDescription;
+            
+            if (AreaImage != null)
+            {
+                var file = Path.Combine(_environment.ContentRootPath, "areaImages", AreaImage.FileName);
+                using (var fileStream = new MemoryStream())
+                {
+                    AreaImage.CopyTo(fileStream);
+                    var fileBytes = fileStream.ToArray();
+
+                    area.AreaImage = fileBytes;
+
+                    if (!ModelState.IsValid)
+                    {
+                        return Page();
+                    }
+                }
+            }
+
+            _context.Add(area);
+            _context.SaveChanges();
+
+            return RedirectToAction("/Get");
         }
     }
 }
