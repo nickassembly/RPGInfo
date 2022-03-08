dfbfb0c7204e37ed845194fcbace650a24b38ed2
nickguerra <nickg@maliciousgames.com>
2022-03-07 18:11:39 -0600

    Added data for arealist and area detail pages

--- RPGInfo.Web/Pages/Areas/AreaDetail.cshtml.cs
+++ RPGInfo.Web/Pages/Areas/AreaDetail.cshtml.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RPGInfo.Web.Data;
+using RPGInfo.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +13,24 @@ namespace RPGInfo.Web.Pages
 {
     public class AreaDetailModel : PageModel
     {
-        
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<AreaDetailModel> _logger;
+
+        public AreaDetailModel(ApplicationDbContext context, ILogger<AreaDetailModel> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [BindProperty]
+        public Area Area { get; set; }
+
+        public void OnGet(int id)
+        {
+            Area = _context.AreasOfInterest.Where(x => x.Id == id).FirstOrDefault();
+
+            Area.AreaNotes = _context.Notes.Where(note => note.AreaId == id).ToList();
+        }
+
     }
 }
