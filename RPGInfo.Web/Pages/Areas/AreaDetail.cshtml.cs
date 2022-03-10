97ca7792c1e820e37b415b06fe284a8cd2f0da14
nickguerra <nickg@maliciousgames.com>
2022-03-10 05:24:43 -0600

    Added note to string conversion method to Area code behind files

--- RPGInfo.Web/Pages/Areas/AreaDetail.cshtml.cs
+++ RPGInfo.Web/Pages/Areas/AreaDetail.cshtml.cs
@@ -32,5 +32,84 @@ namespace RPGInfo.Web.Pages
             Area.AreaNotes = _context.Notes.Where(note => note.AreaId == id).ToList();
         }
 
+        [BindProperty]
+        public string[] AreaNoteStrings { get; set; }
+
+        public ActionResult OnPostAddNotes([FromBody] string[] noteStrings)
+        {
+            List<Note> newNotes = ConvertStringsToNotes(noteStrings);
+
+            _context.Notes.AddRange(newNotes);
+            _context.SaveChanges();
+
+            Area.AreaNotes.AddRange(newNotes);
+
+            return RedirectToPage();
+        }
+
+        public ActionResult OnPutEditNote(Note editedNote)
+        {
+            var noteToEdit = _context.Notes.Where(note => note.Id == editedNote.Id).FirstOrDefault();
+
+            noteToEdit.NoteContent = editedNote.NoteContent != null ? editedNote.NoteContent : noteToEdit.NoteContent;
+            noteToEdit.NoteTitle = editedNote.NoteTitle != null ? editedNote.NoteTitle : noteToEdit.NoteTitle;
+            noteToEdit.NoteDate = editedNote.NoteDate;
+            _context.SaveChanges();
+
+            return RedirectToPage();
+        }
+
+        public ActionResult OnPutDeleteNote(Note noteToDelete)
+        {
+            var noteToRemove = _context.Notes.Where(note => note.Id == noteToDelete.Id).FirstOrDefault();
+
+            if (noteToRemove != null)
+            {
+                _context.Notes.Remove(noteToRemove);
+                _context.SaveChanges();
+            }
+
+            return RedirectToPage();
+        }
+
+        private List<Note> ConvertStringsToNotes(string[] stringsToConvert)
+        {
+            List<Note> newNotes = new List<Note>();
+
+            for (int i = 0; i < stringsToConvert.Length; i++)
+            {
+                Note note = new Note();
+
+                string titleKey = "Title:";
+                string dateKey = "Date:";
+                string contentKey = "Note:";
+                int stringLength = stringsToConvert[i].Length;
+
+                int titleIndex = stringsToConvert[i].IndexOf(titleKey);
+                int dateIndex = stringsToConvert[i].IndexOf(dateKey);
+                int contentIndex = stringsToConvert[i].IndexOf(contentKey);
+
+                string extractedTitle = stringsToConvert[i][titleIndex..dateIndex];
+                string extractedDate = stringsToConvert[i][dateIndex..contentIndex];
+                string extractedContent = stringsToConvert[i][contentIndex..stringLength];
+
+                note.NoteTitle = extractedTitle.Substring(6);
+
+                DateTime noteDate;
+                if (DateTime.TryParse(extractedDate.Substring(5), out noteDate))
+                    note.NoteDate = noteDate;
+                else
+                    note.NoteDate = DateTime.MinValue;
+
+                note.NoteContent = extractedContent.Substring(5);
+
+                note.CharacterId = Area.Id;
+
+                newNotes.Add(note);
+            }
+
+            return newNotes;
+        }
+
     }
 }
