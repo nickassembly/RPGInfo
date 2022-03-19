efb32fa071009ef907168c70e520788df6dd2592
nickguerra <nickg@maliciousgames.com>
2022-03-19 07:29:34 -0500

    Added tabbed page for character display

--- RPGInfo.Web/ViewModels/NoteViewModel.cs
+++ RPGInfo.Web/ViewModels/NoteViewModel.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace RPGInfo.Web.ViewModels
-{
-    public class NoteViewModel
-    {
-        public string NoteTitle { get; set; }
-        public string NoteContent { get; set; }
-    }
-}
+//namespace RPGInfo.Web.ViewModels
+//{
+//    public class NoteViewModel
+//    {
+//        public string NoteTitle { get; set; }
+//        public string NoteContent { get; set; }
+//    }
+//}
--- RPGInfo.Web/ViewModels/RelatedCharacterViewModel.cs
+++ RPGInfo.Web/ViewModels/RelatedCharacterViewModel.cs
@@ -1,20 +1,15 @@
-﻿using RPGInfo.Web.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using RPGInfo.Web.Models;
 
-namespace RPGInfo.Web.ViewModels
-{
-    public class RelatedCharacterViewModel
-    {
-        public byte[] RelatedCharacterPortrait { get; set; }
-        public string RelatedCharacterName { get; set; }
-        public string RelatedCharacterRace { get; set; }
-        public string RelatedCharacterClass { get; set; }
-        public string RelatedCharacterLocation { get; set; }
-        public Note RelatedCharacterNotes { get; set; }
+//namespace RPGInfo.Web.ViewModels
+//{
+//    public class RelatedCharacterViewModel
+//    {
+//        public byte[] RelatedCharacterPortrait { get; set; }
+//        public string RelatedCharacterName { get; set; }
+//        public string RelatedCharacterRace { get; set; }
+//        public string RelatedCharacterClass { get; set; }
+//        public string RelatedCharacterLocation { get; set; }
+//        public Note RelatedCharacterNotes { get; set; }
       
-    }
-}
+//    }
+//}
