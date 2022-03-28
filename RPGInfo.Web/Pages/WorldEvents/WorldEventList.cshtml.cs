bed00e584ed7e39d5d72b94a3ff94d396991010f
nickguerra <nickg@maliciousgames.com>
2022-03-28 17:44:47 -0500

    Added world event list form

--- RPGInfo.Web/Pages/WorldEvents/WorldEventList.cshtml.cs
+++ RPGInfo.Web/Pages/WorldEvents/WorldEventList.cshtml.cs
@@ -1,8 +1,12 @@
 ﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RPGInfo.Web.Data;
+using RPGInfo.Web.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,6 +22,20 @@ namespace RPGInfo.Web.Pages
         {
             _context = context;
             _environment = environment;
+        }
+
+        [BindProperty]
+        public WorldEvent WorldEvent { get; set; }
+
+        [NotMapped]
+        [BindProperty]
+        public IFormFile EventImage { get; set; }
+
+        public List<WorldEvent> WorldEventList { get; set; } = new List<WorldEvent>();
+
+        public void OnGet()
+        {
+            WorldEventList = _context.WorldEvents.ToList();
         }
 
 
