ba8b6edbf3da2e5d16e572b8be54683586fae492
nickguerra <nickg@maliciousgames.com>
2022-06-04 08:08:19 -0500

    finished create method in user code behind

--- RPGInfo.Web/Pages/User/User.cshtml.cs
+++ RPGInfo.Web/Pages/User/User.cshtml.cs
@@ -1,21 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using RPGInfo.Web.Models;
 
 namespace RPGInfo.Web.Pages.User
 {
     public class UserDetailModel : PageModel
     {
-        // TODO: Need to translate from controller to page model (if possible)
-        // 6:35
+        private UserManager<ApplicationUser> _userManager;
+        public UserDetailModel(UserManager<ApplicationUser> userManager)
+        {
+            userManager = _userManager;
+        }
 
-        public IActionResult Index()
+        public IActionResult Create()
         {
             return Page();
         }
+
+        public async Task<IActionResult> OnPostCreate(Models.User user)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser appUser = new ApplicationUser
+                {
+                    UserName = user.Name,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(appUser, user.Password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return RedirectToPage(user);
+        }
+
+
     }
 }
